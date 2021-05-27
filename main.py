fc714cc0c239462f003be74ef26bea28e4079dbb
RyuuichiTempest <ryudo@otaku-cafe.de>
2021-05-27 04:48:46 +0200

    Added newline at end of line

--- main.py
+++ main.py
@@ -35,20 +35,23 @@ print("Welcome to Rock, Paper, Scissors\n")
 
 while game_on is True:
 
-    user_choice = int(input('\nWhat do you choose? "0" for rock, "1" for paper and "2" for scissors\n\n'))
+    user_choice = int(input(
+        '\nWhat do you choose? "0" for rock, "1" for paper and "2" for scissors\n\n'))
 
     if user_choice >= 3:
         print("\nYou should choose a number between 0, 1 and 2\n")
         continue_playing = input('Want to continue? "Y" or "N"?\n\n').lower()
         if continue_playing == "y":
-            user_choice = int(input('What do you choose? "0" for rock, "1" for paper and "2" for scissors\n\n'))
+            user_choice = int(input(
+                'What do you choose? "0" for rock, "1" for paper and "2" for scissors\n\n'))
         else:
             break
 
     print(f"\nYou choose: {rock_paper_scissors_list[user_choice]}\n")
 
     computer_choice = random.randint(0, 2)
-    print(f"\nThe computer chooses: {rock_paper_scissors_list[computer_choice]}\n")
+    print(
+        f"\nThe computer chooses: {rock_paper_scissors_list[computer_choice]}\n")
 
     if user_choice == computer_choice:
         print("It's a draw!\n")
